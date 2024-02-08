cb18d93d9217ecb758c20fa62d10787e74dbe28e
mash9756 <mash9756@Ubuntu-VM.myguest.virtualbox.org>
2024-02-08 09:20:52 -0500

    SCHED_FIFO cleanup

--- SCHED_FIFO/pthread.c
+++ SCHED_FIFO/pthread.c
@@ -22,32 +22,26 @@ typedef struct
     int threadIdx;
 } threadParams_t;
 
-
-// POSIX thread declarations and scheduling attributes
-//
-pthread_t threads[2];
-threadParams_t threadParams[2];
-
-
+/* Main thread declarations and sched attributes */
 pthread_attr_t main_attr;
 struct sched_param main_param;
 pid_t main_pid;
 
+/* Increment thread declarations and sched attributes */
 pthread_t inc_thread;
 threadParams_t inc_thread_params;
 pthread_attr_t inc_attr;
 struct sched_param inc_param;
 
+/* Decrement thread declarations and sched attributes */
 pthread_t dec_thread;
 threadParams_t dec_thread_params;
 pthread_attr_t dec_attr;
 struct sched_param dec_param;
 
-int rt_max_prio, rt_min_prio;
-
-// Unsafe global
-int gsum = 0;
-int numberOfProcessors;
+int rt_max_prio = 0;
+int rt_min_prio = 0;
+int gsum        = 0;
 
 void print_scheduler(void)
 {
@@ -75,10 +69,10 @@ void *incThread(void *threadp)
     int i;
     threadParams_t *threadParams = (threadParams_t *)threadp;
 
-    for(i=0; i<COUNT; i++)
+    for(i = 0; i < COUNT; i++)
     {
-        gsum=gsum+i;
-        printf("\nIncrement thread idx=%d, gsum=%d", threadParams->threadIdx, gsum);
+        gsum = gsum + i;
+        printf("\nIncrement thread idx = %d, gsum = %d", threadParams->threadIdx, gsum);
     }
 }
 
@@ -87,17 +81,18 @@ void *decThread(void *threadp)
     int i;
     threadParams_t *threadParams = (threadParams_t *)threadp;
 
-    for(i=0; i<COUNT; i++)
+    for(i = 0; i < COUNT; i++)
     {
-        gsum=gsum-i;
-        printf("\nDecrement thread idx=%d, gsum=%d", threadParams->threadIdx, gsum);
+        gsum = gsum - i;
+        printf("\nDecrement thread idx = %d, gsum = %d", threadParams->threadIdx, gsum);
     }
 }
 
+/* configure main thread scheduling policy and parameters */
 void set_main_sched(void) {
     int rc      = 0;
     int scope   = 0;
-    main_pid = getpid();
+    main_pid    = getpid();
 
     rt_max_prio = sched_get_priority_max(SCHED_FIFO);
     rt_min_prio = sched_get_priority_min(SCHED_FIFO);
@@ -123,82 +118,71 @@ void set_main_sched(void) {
         printf("\nPTHREAD SCOPE UNKNOWN");
     }
 
-    printf("\nrt_max_prio=%d", rt_max_prio);
-    printf("\nrt_min_prio=%d\n", rt_min_prio);
+    printf("\nrt_max_prio = %d", rt_max_prio);
+    printf("\nrt_min_prio = %d\n", rt_min_prio);
 }
 
+/* configure increment thread scheduling policy and parameters */
 void set_inc_sched(void) {
     int coreid  = 0;
     cpu_set_t threadcpu;
 
     CPU_ZERO(&threadcpu);
-    printf("\nSetting Increment thread to core %d", coreid);
     CPU_SET(coreid, &threadcpu);
-    if(CPU_ISSET(coreid, &threadcpu)) {
-        printf("\nCPU-%d ", 0);
-    }
+    printf("\nIncrement thread set to run on core %d", coreid);
 
     pthread_attr_init(&inc_attr);
     pthread_attr_setinheritsched(&inc_attr, PTHREAD_EXPLICIT_SCHED);
     pthread_attr_setschedpolicy(&inc_attr, SCHED_FIFO);
     pthread_attr_setaffinity_np(&inc_attr, sizeof(cpu_set_t), &threadcpu);
 
-    inc_param.sched_priority=rt_max_prio-1;
+    inc_param.sched_priority=rt_max_prio - 1;
     pthread_attr_setschedparam(&inc_attr, &inc_param);
+
+    inc_thread_params.threadIdx = 0;
 }
 
+/* configure decrement thread scheduling policy and parameters */
 void set_dec_sched(void) {
     int coreid  = 0;
     cpu_set_t threadcpu;
 
     CPU_ZERO(&threadcpu);
-    printf("\nSetting Decrement thread to core %d", coreid);
     CPU_SET(coreid, &threadcpu);
-    if(CPU_ISSET(coreid, &threadcpu)) {
-        printf("\nCPU-%d ", 0);
-    }
+    printf("\nDecrement thread set to run on core %d\n", coreid);
 
     pthread_attr_init(&dec_attr);
     pthread_attr_setinheritsched(&dec_attr, PTHREAD_EXPLICIT_SCHED);
     pthread_attr_setschedpolicy(&dec_attr, SCHED_FIFO);
     pthread_attr_setaffinity_np(&dec_attr, sizeof(cpu_set_t), &threadcpu);
 
-    dec_param.sched_priority=rt_max_prio-2;
+    dec_param.sched_priority = rt_max_prio - 2;
     pthread_attr_setschedparam(&dec_attr, &dec_param);
+
+    dec_thread_params.threadIdx = 1;
 }
 
 int main (int argc, char *argv[])
 {
     cpu_set_t allcpuset;
+    int num_processors = get_nprocs_conf();
 
-    printf("\nThis system has %d processors configured and %d processors available.", get_nprocs_conf(), get_nprocs());
-
-    numberOfProcessors = get_nprocs_conf(); 
-    printf("\nnumber of CPU cores=%d", numberOfProcessors);
-
+    printf("\nThis system has %d processors configured and %d processors available.", num_processors, get_nprocs());
     CPU_ZERO(&allcpuset);
-
-    for(int i = 0; i < numberOfProcessors; i++)
+    for(int i = 0; i < num_processors; i++)
         CPU_SET(i, &allcpuset);
-
-    numberOfProcessors = NUM_CPUS;
-    printf("\nUsing DEFAULT number of CPUS = %d", numberOfProcessors);
+    num_processors = NUM_CPUS;
+    printf("\nRunning all threads on %d CPU core(s)", num_processors);
 
     set_main_sched();
-    set_inc_sched();
 
-    inc_thread_params.threadIdx = 0;
-    pthread_create(&inc_thread,   // pointer to thread descriptor
-                    &inc_attr,     // use default attributes
-                    incThread, // thread function entry point
-                    (void *)&inc_thread_params // parameters to pass in
-                    );
+    set_inc_sched();
+    pthread_create(&inc_thread, &inc_attr, incThread, (void *)&inc_thread_params);
 
     set_dec_sched();
-
-    dec_thread_params.threadIdx = 1;
     pthread_create(&dec_thread, &dec_attr, decThread, (void *)&dec_thread_params);
 
+/* wait for threads to complete */
     pthread_join(inc_thread, NULL);
     pthread_join(dec_thread, NULL);
     
